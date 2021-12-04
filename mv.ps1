2d4afcc2187aa6a9433bff03863964cbf64165eb
pavol kutaj <pkutaj@gmail.com>
2021-12-04 03:54:32 +0000

    <feat> script and template

--- mv.ps1
+++ mv.ps1
@@ -1,4 +1,22 @@
-function create-event($year,$title,$content){
-
+function create-event{
+    param (
+        [Parameter(Mandatory=$true)][string]$year,
+        [Parameter(Mandatory=$true)][string]$title,
+        [string]$content
+    )
+    $template = "${env:MV_FOLDER}\event_template.md"
+    $invalid_characters = "[^-A-Za-z0-9_\.]"
+    $title = "$title" -replace $invalid_characters, "-"
+    $folders = (dir -dir).Name
+    $folders | ForEach-Object {
+        if($_.Substring(0,3) -eq $year.Substring(0,3)) {
+            $new_event_in_software = "${env:MV_FOLDER}\${_}\${year}-${title}.md"
+            New-Item $new_event_in_software
+            Get-Content $template | Set-Content $new_event_in_software
+            if(Read-Host "Open Event? (y/Enter)"){Invoke-Item $new_event_in_software}
+        }
+    }
     
 }
+
+If ($MyInvocation.InvocationName -eq '.') { create-event }
